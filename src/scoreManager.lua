712c374432b83d06cb8c1f6ca02628ce4a71eaed
Austin Andrews <austinandrews6@gmail.com>
2025-08-01 20:11:52 +0100

    Add score manager

---
+++ src/scoreManager.lua
@@ -0,0 +1,25 @@
+local ScoreManager = {}
+
+function ScoreManager:load()
+    self.score = 0
+end
+
+function ScoreManager:addScore(points)
+    -- we want this to be called by the level and have it add a set number of points minus the number of ghosts used
+    self.score = self.score + points
+end
+
+function ScoreManager:reset()
+    self.score = 0
+end
+
+function ScoreManager:getScore()
+    return self.score
+end
+
+function ScoreManager:draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Score: " .. self.score, love.graphics.getWidth() - 110, 10, nil, 2, 2)
+end
+
+return ScoreManager
