96464545d502878f0ef7508ca6b43f4c14a32dbe
Austin Andrews <austinandrews6@gmail.com>
2025-08-02 22:19:41 +0100

    Add platform graphics

--- src/platform.lua
+++ src/platform.lua
@@ -7,6 +7,8 @@ local platformFactory = function(tileSize, tileX, tileY)
     function platform:load()
         self.x = (tileSize * (tileX - 1)) + (tileSize / 2)
         self.y = (tileSize * (tileY - 1)) + (tileSize / 2)
+
+        self.sprite = love.graphics.newImage("images/platform.png")
         self.body = love.physics.newBody(WorldManager:getWorld(), self.x, self.y, "static")
         self.shape = love.physics.newRectangleShape(tileSize, tileSize)
         self.fixture = love.physics.newFixture(self.body, self.shape)
@@ -34,8 +36,11 @@ local platformFactory = function(tileSize, tileX, tileY)
     end
 
     function platform:draw()
-        love.graphics.setColor(0.5, 0.5, 0.5)
-        love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+        love.graphics.setColor(1, 1, 1, 1)
+        local x, y = self.body:getPosition()
+        local angle = self.body:getAngle()
+
+        love.graphics.draw(self.sprite, x, y, angle, 1, 1, self.sprite:getWidth() / 2, self.sprite:getHeight() / 2)
     end
 
     return platform
--- src/worldAttributes.lua
+++ src/worldAttributes.lua
@@ -1,6 +1,6 @@
 local worldAttributes = {
     gravity = 1500,
-    meter = 64
+    meter = 64,
 }
 
 return worldAttributes
