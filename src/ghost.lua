35c77d92e7df9c079decf87c8e2f0fd491742d77
Austin Andrews <austinandrews6@gmail.com>
2025-08-03 13:55:07 +0100

    Fix ghost invulnerability when reset

--- src/ghost.lua
+++ src/ghost.lua
@@ -87,7 +87,7 @@ function Ghost:reset()
     self.collidableGhosts = {}
     self.canCollideWithPlayer = false
     self.physicsEntity.box:setActive(true)
-    self.dead = false
+    self.spawnedAt = love.timer.getTime()
 end
 
 function Ghost:update(dt)
